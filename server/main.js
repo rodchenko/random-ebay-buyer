967340aea190ca433069eaa09174394de63a816b
Valia Fetisov <valyalof@gmail.com>
2016-05-16 19:08:21 +0300

    find and buy item on ebay

---
+++ server/main.js
@@ -0,0 +1,110 @@
+Items = new Mongo.Collection('items');
+
+
+import { Meteor } from 'meteor/meteor';
+import ebay from 'ebay-api'
+
+
+Meteor.startup(() => {
+
+  //
+  // поиск по товарам
+  //
+  var results = search(["art", "painting"]);
+  // console.log('results', results);
+
+
+  //
+  // выбор одного из результатов поиска
+  //
+  var random = Math.floor(results.length * Math.random());
+  var single = results[random];
+  // console.log('single', single, random);
+  console.log('single', single.title, single.galleryPlusPictureURL);
+
+
+  //
+  // сохраняет выбранный товар в базу данных
+  //
+  Items.insert(single);
+
+
+  //
+  // производит покупку товара
+  //
+  buyItem(single.itemId);
+
+
+});
+
+
+function buyItem(itemId) {
+
+  console.log('item ready to buy', itemId);
+  return Meteor.wrapAsync((callback, itemId = itemId) => {
+    ebay.xmlRequest({
+      'serviceName': 'Shopping',
+      'opType': 'GetSingleItem',
+      'appId': Meteor.settings.ebay.appId,
+      params: {
+        'ItemId': itemId
+      }
+    },
+    function(error, data) {
+
+      console.log('item bought!', error, data);
+      callback(error, data);
+
+    });
+  });
+
+}
+
+
+//
+// производит поиск по товарам с помощью api
+//
+function search(parameters) {
+
+  var params = {
+    keywords: parameters,
+    outputSelector: ['AspectHistogram'],
+    paginationInput: {
+      entriesPerPage: 10
+    },
+    itemFilter: [
+      {name: 'FreeShippingOnly', value: true},
+      {name: 'MaxPrice', value: '5'},
+      {name: 'buyItNowAvailable', value: true}
+    ],
+    domainFilter: [
+      {name: 'domainName', value: 'Art'}
+    ]
+  };
+
+  return Meteor.wrapAsync((callback) => {
+    ebay.xmlRequest({
+        serviceName: 'Finding',
+        opType: 'findItemsByKeywords',
+        appId: Meteor.settings.ebay.appId,
+        params: params,
+        parser: ebay.parseResponseJson
+      },
+      // gets all the items together in a merged array
+      function itemsCallback(error, itemsResponse) {
+
+        if (error) throw error;
+
+        var items = itemsResponse.searchResult.item;
+        // items.forEach((item) => {
+          // console.log('item', item.title, item.galleryPlusPictureURL);
+        // });
+
+        callback(null, items);
+
+      }
+    );
+  })();
+
+}
+
